10c57a8ee73b2a40ab9f4b773e11be9a00d55038
Shruthi486 <shruthishruthi6200@gmail.com>
2026-04-24 13:33:04 +0530

    Add files via upload

---
+++ src/Interface/SBI.java
@@ -0,0 +1,8 @@
+package Interface;
+//Using interface before java 1.8
+public interface SBI {
+      void deposite(double amount);
+      void withdrawl(double amount);
+      double checkBalance();
+      void accountDetails();
+}
